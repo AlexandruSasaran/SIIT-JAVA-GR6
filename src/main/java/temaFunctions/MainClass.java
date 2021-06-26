d61d434f1123de2b00d52de2baf64043381c30a6
Alexandru Sasaran <83360202+AlexandruSasaran@users.noreply.github.com>
2021-06-26 13:59:58 +0300

    Update MainClass.java
    Am adaugat randurile: 28 - 66 (bubbleSort method).

--- src/main/java/temaFunctions/MainClass.java
+++ src/main/java/temaFunctions/MainClass.java
@@ -25,7 +25,46 @@ Fac un test pentru mai multi agenti. Am extras codul intr-o metoda.
  */
         testLista2(seller1, seller2);
     }
+/**
+ * Sortare utilizand algoritmul bubble sort
+  */
+        SalesRepresentative[] sortingSellerList = sortingSellerList();
+        MainClass bubble = new MainClass();
+        SalesRepresentative[] sortedList = bubble.sort(sortingSellerList);
+        System.out.println(Arrays.toString(sortedList));
+    }
+/**
+* Metoda bubble sort
+ */
 
+    public SalesRepresentative[] sort(SalesRepresentative[] sellersList){
+        boolean sorted = true;
+        while (sorted){
+            sorted = false;
+            for (int i = 0; i < sellersList.length - 1; i++){
+                if (sellersList[i].getRevenue() < sellersList[i+1].getRevenue()) {
+                    SalesRepresentative temp  = sellersList[i];
+                    sellersList[i] = sellersList[i + 1];
+                    sellersList[i + 1] = temp;
+                    sorted = true;
+                }
+            }
+        }
+        return sellersList;
+    }
+
+    public static SalesRepresentative[] sortingSellerList() {
+        SalesRepresentative seller1 = new SalesRepresentative("Andrei", 10, 500);
+        SalesRepresentative seller2 = new SalesRepresentative("Alexandru", 7, 800);
+        SalesRepresentative seller3 = new SalesRepresentative("Mircea", 10, 600);
+        SalesRepresentative seller4 = new SalesRepresentative("Ionut", 1, 2500);
+        SalesRepresentative seller5 = new SalesRepresentative("Paul", 13, 200);
+        SalesRepresentative seller6 = new SalesRepresentative("Sergiu", 8, 1000);
+
+        SalesRepresentative[] sellersList = new SalesRepresentative[]{seller1, seller2, seller3, seller4, seller5, seller6};
+        return sellersList;
+    }
+    
     private static void testLista2(SalesRepresentative seller1, SalesRepresentative seller2) {
         SalesRepresentative seller3 = new SalesRepresentative("Mircea", 10, 600);
         SalesRepresentative seller4 = new SalesRepresentative("Ionut", 1, 2500);
